d56d32956e7cac70ccde24e9dcdb0b522403f7e3
Sang Duong <sang.duongquangsang@gmail.com>
2026-01-04 20:24:42 +0900

    Kết nối database MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server bắt đầu chạy trên cổng ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server bắt đầu chạy trên cổng ${PORT}`);
+  });
 });
