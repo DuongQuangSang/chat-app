9987c30fda04f5a4d85c6b256b5b2b952848e731
Sang Duong <sang.duongquangsang@gmail.com>
2026-01-08 00:30:15 +0900

    Xong phần Signup

--- shared/index.d.ts
+++ shared/index.d.ts
@@ -12,3 +12,15 @@ export const signInSchema: z.ZodObject<{
   username: z.ZodString;
   password: z.ZodString;
 }>;
+
+export const userBaseSchema: z.ZodObject<{
+  _id: z.ZodString;
+  username: z.ZodString;
+  email: z.ZodString;
+  displayName: z.ZodString;
+  avatarUrl: z.ZodOptional<z.ZodString>;
+  bio: z.ZodOptional<z.ZodString>;
+  phone: z.ZodOptional<z.ZodString>;
+  createdAt: z.ZodOptional<z.ZodString>;
+  updatedAt: z.ZodOptional<z.ZodString>;
+}>;
